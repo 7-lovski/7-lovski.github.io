0fa1280bcc02998809f0158be1d4687a5827ba6b
7-lovski <910309021@qq.com>
2026-05-24 21:16:31 +0800

    界面更新

--- src/utils/projectMedia.ts
+++ src/utils/projectMedia.ts
@@ -1,17 +1,32 @@
-const narrativeProjectMedia = new Map<string, { embed: string }>([
+type NarrativeProjectMedia =
+	| { type: 'embed'; src: string }
+	| { type: 'video'; src: string; poster?: string };
+
+const narrativeProjectMedia = new Map<string, NarrativeProjectMedia>([
 	[
 		'ue-gas-multiplayer',
 		{
-			embed: 'https://player.bilibili.com/player.html?bvid=BV1HqGL6jEhx&autoplay=0',
+			type: 'video',
+			src: 'https://github.com/7-lovski/7-lovski.github.io/releases/download/Media/Crunch.mp4',
+			poster: '/projects/ue-gas-multiplayer/Crunch-poster.png',
 		},
 	],
 ]);
 
-export function getNarrativeEmbedSrc(pathname: string): string | undefined {
-	return getNarrativeProjectMedia(pathname)?.embed;
+export function getNarrativeMedia(pathname: string): NarrativeProjectMedia | undefined {
+	return getNarrativeProjectMedia(pathname);
 }
 
-function getNarrativeProjectMedia(pathname: string): { embed: string } | undefined {
+export function getProjectCoverSrc(slug: string | undefined): string | undefined {
+	if (!slug) {
+		return undefined;
+	}
+
+	const media = narrativeProjectMedia.get(slug);
+	return media?.type === 'video' ? media.poster : undefined;
+}
+
+function getNarrativeProjectMedia(pathname: string): NarrativeProjectMedia | undefined {
 	const normalizedPath = pathname.replace(/\/+$/, '');
 	const slug = normalizedPath.split('/').filter(Boolean).at(-1);
 
